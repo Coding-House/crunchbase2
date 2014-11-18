a23ebdb7916d945f7a2d83b187b5eb4610dada9c
Samer Buna <samer.buna@gmail.com>
2014-11-18 12:42:40 -0800

    Use a function for key param

--- util/url_helper.js
+++ util/url_helper.js
@@ -1,46 +1,43 @@
-var config = require("../config");
-var endpoint = "http://api.crunchbase.com/v/2/";
-var keyParam = "user_key";
-var qs = require("qs");
+var config = require('../config');
+var endpoint = 'http://api.crunchbase.com/v/2/';
+var qs = require('qs');
+
+function keyParam() {
+  return '?user_key=' + config.apikey;
+}
 
 function getOrganizationsUrl(params) {
   return endpoint
-  + 'organizations?' + keyParam + '=' + config.apikey
+  + 'organizations' + keyParam
   + '&' + qs.stringify(params);
 }
 
 function getOrganizationUrl(permalink) {
   return endpoint
-  + 'organization/' + permalink + '?' + keyParam + '=' + config.apikey;
+  + 'organization/' + permalink + keyParam;
 }
 
 function getPeopleUrl(page){
   return endpoint
-  + 'people?'
-  + keyParam + '=' + config.apikey;
-  + "&" + "=" + qs.stringify(page)
+  + 'people' + keyParam
+  + '&' + qs.stringify(page);
 }
 
 function getPersonUrl(permalink){
   return endpoint
-  + 'person/'
-  + permalink
-  + "?"
-  + keyParam + '=' + config.apikey
+  + 'person/' + permalink + keyParam;
 }
 
 function getProductsUrl(params) {
   return endpoint
-  + 'products?' + keyParam + '=' + config.apikey
+  + 'products' + keyParam
   + '&' + qs.stringify(params);
 }
 
 function getProductUrl(permalink) {
   return endpoint
-  + 'product/' + permalink + '?'
-  + keyParam + '=' + config.apikey
+  + 'product/' + permalink + keyParam;
 }
-
 
 module.exports = {
   getOrganizationsUrl: function(params) {
